16529027104c4bd47726141df3b6498b43831302
Sabbardev <sabbar@ppebuddy.com>
2021-02-17 02:11:14 +0500

    CJI Project Added

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html><!--  This site was created in Webflow. http://www.webflow.com  -->
+<!--  Last Published: Thu Dec 10 2020 21:48:51 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="5fc8298d3976af2ebc15062e" data-wf-site="5f8dd38eda8d0fcbfb228fee">
+<head>
+  <meta charset="utf-8">
+  <title>CJI</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/cji-222d60.webflow.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="css/demo.css">
+  <link rel="stylesheet" href="css/noJS.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.1.1/css/mdb.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+  <style>
+.outline-text{
+  -webkit-text-stroke-width: 3px;
+  -webkit-text-stroke-color: rgba(16, 24, 32, 0.6);
+}
+.services-container {
+   position: -webkit-sticky !important;
+   position: sticky !important;
+}
+</style>
+</head>
+<body>
+  <div class="page-wrapper">
+
+    <!-- header section starts here -->
+    <div class="header-wrap">
+      <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand wow slideInLeft" href="javascript:void(0)"><img src="images/logo.png" alt="logo image"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="nav-link" href="javascript:void(0)">About us</a></li>
+              <li class="nav-item"><a class="nav-link" href="javascript:void(0)">Services</a></li>
+              <li class="nav-item"><a class="nav-link" href="javascript:void(0)">Categories</a></li>
+              <li class="nav-item"><a class="nav-link" href="javascript:void(0)">Contact Us</a></li>
+              </ul>
+          </div>
+      </nav>
+  </div>
+  <!-- header section ends here -->
+  <!-- banner section starts here -->
+  <div class="banner-wrapper">
+      <video autoplay muted loop id="shipvedio">
+          <source src="images/banner.mov" type="video/mp4">
+      </video>
+      <div class="vedio-banner-text">
+          <h4>We are</h4>
+          <h1>CJI trading</h1>
+          <p>A trailblazer in leadership and quality, CJI Trading LLC is a visionary in supply-chain logistics, strategic management, product development and marketing strategy. From its infancy, CJI Trading LLC has prioritized growth and visibility for its brands, while elevating quality and vision, year after year. The company’s success today can be attributed to its unwavering purpose to create, manufacture and distribute a wide variety of commodities, which help enrich the lives of their loyal customer base worldwide.</p>
+          <p>With the dedication and direction of strong leadership and a forward-thinking team of talents, CJI Trading LLC remains unstoppable in their desire to continuously reach new heights, each and every year.</p>
+      </div>
+      <div class="banner-overlay"></div>
+  </div>
+  <!-- banner section ends here -->
+    <div id="services" class="section services">
+      <div data-w-id="109b805b-fa70-6a13-101d-1b125eaf5cf6" class="services-scroll-track">
+        <div class="services-container">
+          <div class="borderbg-image"><img src="images/banner-services_1.png" loading="lazy" width="1440" sizes="100vw" srcset="images/banner-services_1-p-500.png 500w, images/banner-services_1-p-800.png 800w, images/banner-services_1-p-1080.png 1080w, images/banner-services_1-p-1600.png 1600w, images/banner-services_1-p-2000.png 2000w, images/banner-services_1-p-2600.png 2600w, images/banner-services_1-p-3200.png 3200w, images/banner-services_1.png 4389w" alt="" class="border-image"></div>
+          <div class="services-images-container">
+            <div class="service-image"><img src="images/photo1-1.png" alt="">
+              <h1 class="service-text textcolor-black">SERVICES</h1>
+              <h4 class="service-image-text">Warehouse And Fulfillment Center</h4>
+            </div>
+            <div class="service-image _2"><img src="images/photo2-1.png" alt="">
+              <h1 class="service-text-2 textcolor-black outline-text">SERVICES</h1>
+              <h4 class="service-image-text">Customer Support</h4>
+            </div>
+            <div class="service-image _3"><img src="images/photo3-1.png" alt="">
+              <h4 class="service-image-text">Marketing Team</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div data-w-id="607761d0-4b2c-e7af-b734-018221b59b49" class="services-scroll-track-mobile">
+        <div class="services-container">
+          <div class="borderbg-image"><img src="images/banner-services.png" loading="lazy" width="1440" sizes="100vw" srcset="images/banner-services-p-500.png 500w, images/banner-services-p-800.png 800w, images/banner-services-p-1080.png 1080w, images/banner-services-p-1600.png 1600w, images/banner-services-p-2000.png 2000w, images/banner-services-p-2600.png 2600w, images/banner-services.png 2880w" alt="" class="border-image"></div>
+          <div class="services-images-container">
+            <div class="service-image"><img src="images/photo1-1.png" loading="lazy" alt="" class="image-mobile-size-big">
+              <h1 class="service-text textcolor-black">SERVICES</h1>
+              <h4 class="service-image-text">Warehouse And Fulfillment Center</h4>
+            </div>
+            <div class="service-image _2"><img src="images/photo2-1.png" loading="lazy" alt="" class="image-mobile-size-big">
+              <h1 class="service-text-2 textcolor-black outline-text">SERVICES</h1>
+              <h4 class="service-image-text">Customer Support</h4>
+            </div>
+            <div class="service-image _3"><img src="images/photo3-1.png" loading="lazy" alt="" class="image-mobile-size-big">
+              <h4 class="service-image-text">Marketing Team</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- carosel sections starts here-->
+    <div class="carosel-wrapper">
+        <div class="carosel-heading">
+        <h2>SERVICES </h2>
+        <span>SERVICES</span> 
+        </div>
+        <div class="border-bg-back">
+            <img src="images/carosel-back.png" alt="">
+        </div>
+            <div class="owl-carousel">
+                <div class="service-wrap">
+                    <img src="images/service1.png" alt="">
+                    <h3>Warehouse  And<br> 
+                        Fulfillment Center</h3>
+                </div>
+                <div class="service-wrap">
+                    <img src="images/service2.png" alt="">
+                    <h3>Customer Support</h3>
+                </div>
+                <div class="service-wrap">
+                    <img src="images/service3.png" alt="">
+                    <h3>Marketing Team</h3>
+                </div>
+            </div>
+    </div>
+    <!-- our categories section starts here -->
+    <!-- content section starts here -->
+    <div class="content-wrapper">
+      <section class="categories-wrap">
+          <div class="container">
+              <h2>Our Categories</h2>
+              <ul id="da-thumbs" class="da-thumbs">
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/watch.png" />
+                          <div><h6>watches</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/eyewear.png" />
+                          <div><h6>eyewear</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/KITCHENWARE.png" />
+                          <div><h6>Kitchenware</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/jewelry.png" />
+                          <div><h6>jewelry</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">	
+                      <a href="javascript:void(0)">
+                          <img src="images/footwear.png" />
+                          <div><h6>footwear</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/babyproducts.png" />
+                          <div><h6>baby products</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/handbags&leathergoods.png" />
+                          <div><h6>handbags & leather goods</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/Household.png" />
+                          <div><h6>Household</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/Apparel.png" />
+                          <div><h6>Apparel</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/electronics.png" />
+                          <div><h6>electronics</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/pens.png" />
+                          <div><h6>pens</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/Outoorandsportinggoods.png" />
+                          <div><h6>Outdoor and sporting goods</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/medical.png" />
+                          <div><h6>medical</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInLeft">
+                      <a href="javascript:void(0)">
+                          <img src="images/food&beverage.png" />
+                          <div><h6>food & beverage</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/healt&beauty.png" />
+                          <div><h6>Health & beauty</h6></div>
+                      </a>
+                  </li>
+                  <li class="wow slideInRight">
+                      <a href="javascript:void(0)">
+                          <img src="images/toys.png" />
+                          <div><h6>toys</h6></div>
+                      </a>
+                  </li>
+              </ul>
+          </div>
+      </section>
+      <!-- our categories section ends here -->
+  </div>
+  <!-- conatct section starts here -->
+  <div class="contact-wrap">
+      <h2 class="form-head-title">Contact us</h2>
+      <!-- form  starts-->
+      <div class="form-wrapper">
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $message = $_POST['message'];
+            $msg = '';
+            $msg .= '<b>Name : </b>' . $name . '<br />';
+            $msg .= '<b>E-mail : </b>' . $email . '<br />';
+            $msg .= '<b>phone : </b>' . $phone . '<br />';
+            $msg .= '<b>message : </b>' . $message . '<br />';
+            $to = "sabbar@thedigitalartistry.com";
+            $subject = "The Digital Artistery";
+                // Always set content-type when sending HTML email
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                // More headers
+                $headers .= 'From: <webmaster@example.com>' . "\r\n";
+                //$headers .= 'Cc: myboss@example.com' . "\r\n";
+                mail($to,$subject,$msg,$headers);
+            echo "<h2>Your message has been sent</h2>";
+        }else{ ?>
+          <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+              <h3>Get in touch</h3>
+              <div class="form-group md-form">
+                  <input type="text" class="form-control" id="name" name="name" maxlength="256" required="">
+                  <label>Name</label>
+              </div>
+              <div class="form-group md-form">
+                  <input type="email" class="form-control" id="email" name="email" maxlength="256" required="">
+                  <label>E-mail</label>
+              </div>
+              <div class="form-group md-form">
+                  <input type="tel" class="form-control" id="phone" name="phone" pattern='^\+?\d{0,13}'  required="">
+                  <label>Phone</label>
+              </div>
+              <div class="form-group md-form">
+                  <input type="text" class="form-control" id="message" name="message" maxlength="5000"  required="" data-name="Contact Message">
+                  <label>Message</label>
+              </div>
+              <button type="submit" class="btn btn-primary">send message</button>
+          </form>
+          <?php } ?>
+      </div>
+      <!-- forms ends -->
+  </div>
+  <!-- conatct section ends here -->
+  <!-- footer section starts here -->
+  <div class="footer-wrap">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-sm-6">
+                  <div class="cji-image">
+                      <img src="images/cji-trading.png" alt="">
+                  </div>
+              </div>
+              <div class="col-sm-6">
+                  <div class="cji-text">
+                      <h5>contact:</h5>
+                      <h5>Info@cjillc.com</h5>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- footer section ends here -->
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5f8dd38eda8d0fcbfb228fee" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script type="text/javascript">
+particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#aee64e"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.25,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#f79500","opacity":0.3,"width":1},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+</script>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.1.1/js/mdb.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.hoverdir.js"></script>
+<script src="js/modernizr.custom.97074.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+<script>
+    new WOW().init();
+        $(function() {
+        
+            $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+
+        });
+</script>
+</body>
+</html>
---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                margin: 20,
+
+            },
+            480:{
+                items:2,
+                margin: 20,
+                nav:false,
+            },
+            767:{
+                items:2,
+                margin: 20,
+                nav:false,
+            }
+        }
+    });
+    
+  });
